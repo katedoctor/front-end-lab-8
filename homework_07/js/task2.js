324cd8baa92795788a01112e96e409318fb43631
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-02-13 23:06:46 +0200

    Add files via upload

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,39 +1,44 @@
 let play = confirm(`Do you want to play a game?`);
-let random = 6;
-let prize = 10;
 let maxPrize = 10;
+let prize = 10;
 let totalPrize = 0;
-
+let min = 0;
+let max = 5;
 
 if (play) {
-  let luckyNum =  Math.floor(Math.random() * random);
+  let luckyNum =
+    Math.floor(Math.random() * (max - min + 1)) + min;
   for (let i = 0; i < 3; i++) {
-    let userNum = Number(prompt(
-      `Enter a number from 0 to ${random - 1}\n` + `Attempts left: ${3 - i}\n` + `Total prize: ${totalPrize}$\n` + `Possible prize on current attempt: ${prize}$`
-    ));
+    let userNum = Number( prompt(
+        `Enter a number from ${min} to ${max}\n` + `Attempts left: ${3 - i}\n` + `Total prize: ${totalPrize}$\n` + `Possible prize on current attempt: ${prize}$`
+      )
+    );
     if (userNum === luckyNum) {
       totalPrize += prize;
       alert( `Thank you for a game. Your prize is: ${prize}$\n` + `Total prize: ${totalPrize}$` );
       play = confirm(`Do you want to continue a game?`);
       if (play) {
-        random *= 2;
+        min *= 2;
+        max *= 2;
         maxPrize *= 3;
         prize = maxPrize;
         i = -1;
-        luckyNum =  Math.floor(Math.random() * random);
+        numRandom =
+          Math.floor(Math.random() * (max - min + 1)) + min;
       } else {
         break;
       }
     } else if (i == 2) {
-      play = confirm(`Thank you for a game. Your prize is: ${totalPrize}$\n` + `Do you want to play again?`);
+      play = confirm( `Thank you for a game. Your prize is: ${totalPrize}$\n` + `Do you want to play again?` );
       if (play) {
         totalPrize = 0;
         maxPrize = 10;
         prize = 10;
-        random = 6;
+        min = 0;
+        max = 5;
         i = -1;
         luckyNum =
-          Math.floor(Math.random() * random);
+          Math.floor(Math.random() * (max - min + 1)) + min;
       } else {
         break;
       }
