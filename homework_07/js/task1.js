bce9b5177607e15731f9aa068fea56bd3452e299
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-03-07 02:31:18 +0200

    Update task1.js

--- homework_07/js/task1.js
+++ homework_07/js/task1.js
@@ -1,23 +1,20 @@
-window.onload = function (){
+function (){
   var a = prompt("Please, enter number from 1 to 20",1);
   var count = parseInt(a);
   var brackets = ' ';
 
-
-      if ( count >= 1 && count < 21){
-            for (var i = 1; i <= count; i++){
-              for ( var j = 1; j <= (count - i); j++){
-                brackets = brackets.concat('   ');
-              }
-              for ( var q = 2; q <= i; q++){
-                brackets = brackets.concat('[~][~]');
+ if ( count >= 1 && count < 21){
+        for (var i = 1; i <= count; i++){
+          for ( var j = 1; j <= (count - i); j++){
+            brackets = brackets.concat('   ');
             }
-              brackets = brackets.concat('[~]\n ');
+          for ( var q = 2; q <= i; q++){
+            brackets = brackets.concat('[~][~]');
             }
-
+            brackets = brackets.concat('[~]\n ');
+            }
               console.log(brackets);
       } else {
-        
-          console.error(" Incorrect data");
+              console.error(" Incorrect data");
       }
 }
