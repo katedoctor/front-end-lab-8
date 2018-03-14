41035bb08a5a7edf3857fd1ac13227e282f255e6
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-03-14 16:57:05 +0200

    Add files via upload

--- onlain/homework_10/task1.js
+++ onlain/homework_10/task1.js
@@ -1,1 +1,34 @@
+function debounce(callback, time) {
+  let timeout = 0;
+  return () => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(callback, time);
+  };
+}
 
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();// Should print 'Iterator value 2'
