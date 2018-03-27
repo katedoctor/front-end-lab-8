eb0c4e91e031bf002d3dee00a7c261ac8a7c87c8
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-03-27 23:27:39 +0300

    Add files via upload

---
+++ homework_05/tower-of-babel/babel_setup.js
@@ -0,0 +1,3 @@
+console.log("Hello Babel");
+console.log(`Hello ${process.argv[2]}`);
+//babel - node my - program.js Babel
--- homework_05/tower-of-babel/class_extend.js
+++ homework_05/tower-of-babel/class_extend.js
@@ -4,7 +4,7 @@ class Character {
     this.y = y;
     this.health_ = 100;
   }
-  damage(){
+  damage() {
     this.health_ = this.health_ - 10;
   }
   getHealth() {
@@ -14,8 +14,26 @@ class Character {
     return `x: ${this.x} y: ${this.y} health: ${this.getHealth()}`;
   }
 }
+class Player extends Character {
+  constructor(x, y, name){
+    super(x, y);
+    this.name = name;
+  }
+  move(dx, dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+  toString() {
+    return `name: ${this.name} ${super.toString()}`;
+  }
+}
 var x = process.argv[2];
 var y = process.argv[3];
+var name = process.argv[4];
 var character = new Character(+x, +y);
 character.damage();
 console.log(character.toString());
+var player = new Player(+x, +y, name);
+player.damage();
+player.move(7, 8);
+console.log(player.toString());
