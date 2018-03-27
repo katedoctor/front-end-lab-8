9cb5e62ba162b1c643ac83e417dff63245c9a8f2
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-03-27 21:30:56 +0300

    Add files via upload

--- homework_04/script/script.js
+++ homework_04/script/script.js
@@ -1,20 +1,3 @@
-// TODO: remove layout comments before submitting homework
-// Task 1
-function assign(obj){
-  for(let i = 1; i<arguments.length; i++) {
-    var src = arguments[i];
-    if (src === undefined || src === null) continue;
-    for ( let key in src) {
-      if(src.hasOwnProperty(key)) {
-        obj[key]=src[key];
-      }
-    }
-  }
-  return obj;
-}
-
-
-
 function Config({ name, attack, hitpoints }) {
   this.name = name;
   this.attack = attack;
@@ -22,15 +5,15 @@ function Config({ name, attack, hitpoints }) {
   this.currentHitpoints = hitpoints;
 }
 
-Config.prototype.getHitpoints = function() {
+Config.prototype.getHitpoints = function () {
   return this.currentHitpoints;
- }
+}
 
-Config.prototype.setHitpoints = function(num) {
+Config.prototype.setHitpoints = function (num) {
   this.currentHitpoints = num;
 }
 
-Config.prototype.getTotalHitpoints = function() {
+Config.prototype.getTotalHitpoints = function () {
   return this.hitpoints;
 }
 
@@ -38,30 +21,30 @@ Config.prototype.setTotalHitpoints = function (num) {
   this.hitpoints = num;
 }
 
-Config.prototype.getAttack = function() {
+Config.prototype.getAttack = function () {
   return this.attack;
 }
 
-Config.prototype.setAttack = function(num) {
+Config.prototype.setAttack = function (num) {
   this.attack = num;
 }
 
-Config.prototype.fight = function(enemy) {
-  if(enemy.isAlive() && this !== enemy) {
-    if(this.countFight){
-      enemy.currentHitpoints -= this.attack*2;
+Config.prototype.fight = function (enemy) {
+  if (enemy.isAlive() && this !== enemy) {
+    if (this.countFight) {
+      enemy.currentHitpoints -= this.attack * 2;
       this.countFight--;
-    }else{
-    enemy.currentHitpoints -= this.attack;
+    } else {
+      enemy.currentHitpoints -= this.attack;
     }
-  }else if(!enemy.isAlive()){
+  } else if (!enemy.isAlive()) {
     this.gets1(enemy);
   }
 }
 
 
-Config.prototype.isAlive = function() {
- return this.hitpoints>0;
+Config.prototype.isAlive = function () {
+  return this.hitpoints > 0;
 }
 
 
@@ -73,22 +56,22 @@ function Champion({ name, attack, hitpoints }) {
   this.currentHitpoints = hitpoints;
   this.hit = false;
 
-  Config.call(this, { name, attack, hitpoints});
+  Config.call(this, { name, attack, hitpoints });
 
-  this.heal = function() {
+  this.heal = function () {
     this.currentHitpoints += 5;
-    if ( this.hitpoints > this.currentHitpoints) {
+    if (this.hitpoints > this.currentHitpoints) {
       this.hitpoints = this.currentHitpoints;
     }
   }
 
-  this.defence = function() {
+  this.defence = function () {
     this.setTotalHitpoints(this.hitpoints + 1);
     this.hit = true;
     return this.hit;
   }
 
-  this.gets1 = function() {
+  this.gets1 = function () {
     this.attack++;
   }
 
@@ -107,30 +90,31 @@ Champion.prototype = Object.create(Config.prototype);
 Champion.prototype.constructor = Champion;
 
 
-function Monster({name, attack, hitpoints}) {
+function Monster({ name, attack, hitpoints }) {
   this.name = name;
   this.attack = attack;
   this.hitpoints = hitpoints;
   this.countFight = 0;
   this.hit = false;
 
+  Config.call(this, { name, attack, hitpoints });
 
 
-  this.enrage = function() {
-  this.countFight = 2;
+  this.enrage = function () {
+    this.countFight = 2;
   }
 
-  this.fury = function() {
-    if(this.hitpoints > 5){
+  this.fury = function () {
+    if (this.hitpoints > 5) {
       this.currentHitpoints -= 5;
       this.hitpoints -= 5;
       this.attack += 2;
     }
   }
 
-  this.gets1 = function(){
-    this.hitpoints = Math.floor(this.hitpoints + enemy.currentHitpoints*0.25);
-    this.currentHitpoints = Math.floor(this.currentHitpoints + enemy.currentHitpoints*0.1);
+  this.gets1 = function () {
+    this.hitpoints = Math.floor(this.hitpoints + enemy.currentHitpoints * 0.25);
+    this.currentHitpoints = Math.floor(this.currentHitpoints + enemy.currentHitpoints * 0.1);
   }
 
   Config.prototype.fight = function (enemy) {
@@ -151,4 +135,3 @@ function Monster({name, attack, hitpoints}) {
 
 Monster.prototype = Object.create(Config.prototype);
 Monster.prototype.constructor = Monster;
-
