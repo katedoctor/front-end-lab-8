bcdb29c5dced447ee7aa4584668a51d5c0092637
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-03 09:26:31 +0300

    Add files via upload

--- homework_06/js/task1.js
+++ homework_06/js/task1.js
@@ -1,1 +1,20 @@
-
+const http = {
+  get: function(url) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    });
+  },
+  post: function(url, requestBody) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send(requestBody);
+    });
+  }
+};
--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,1 +1,46 @@
+const btn = document.getElementById("btnIP"),
+      btnResponse = document.getElementById("btn-response"),
+      ip = document.getElementById("form-input"),
+      respon = document.getElementById("response-result");
+let response;
 
+btn.onclick = function() {
+  if(validateIP(ip)){
+  const url = `https://ipapi.co/${ip.value}/json/`;
+  http.get(url).then(result => {
+    response = result;
+    createTable(JSON.parse(result));
+  });
+  }else{
+    alert('Error:you have entered an invalid IP address' );
+  }
+}
+
+  function validateIP(ip) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip.value)) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  function createTable(data) {
+    var table = document.createElement("table");
+    var tablePlace = document.getElementById("table");
+     for( var key in data) {
+      table.innerHTML +=
+        `<tr>
+          <td>${key}</td>
+          <td>${data[key]}</td>
+      </tr> `;
+      tablePlace.appendChild(table);
+     }
+    }
+
+
+  btnResponse.onclick = function() {
+    const url = "https://shrouded-garden-94580.herokuapp.com/";
+    http.post(url, response).then((response) => {
+    respon.innerHTML = response;
+    });
+  }
