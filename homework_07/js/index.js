598df9bd5dae2ac202300b33ffb99f164a57d110
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-08 22:27:47 +0300

    Add files via upload

--- homework_07/js/index.js
+++ homework_07/js/index.js
@@ -10,35 +10,39 @@ $(document).ready(function createBoard() {
 
   for(let i=0; i<board_size; i++){
     $('<div>').addClass('data-row').appendTo('.board');
-
     for(let j=0; j<board_size; j++){
-      $('.data-row').eq(i).append('<div class="data-cell">');
-    }
+      $('.data-row').eq(i).append(`<div  class="data-cell">`);
+     }
   }
 
   for(let i=0; i < (board_size - 1); i++) {
     $('<div>').addClass('data-row').appendTo('.dots');
     for(let j=0; j<(board_size - 1); j++) {
-      $('.dots .data-row').eq(i).append('<div class ="data-dots">');
+      $('.dots .data-row').eq(i).append(`<div id='${j}'class ="data-dots">`);
     }
   }
 
 $(`.data-dots`).one('click', (function(e){
-  let ring = e.target;
+  let ring = e.target,
+      id = $(this).attr("id");
   if(move%2 ===0) {
     $(ring).addClass(`white`);
-    console.log(ring);
+    console.log($(this).parent().prev().prev().children(`#${id}`));
     $(ring).css({ 'background': 'white' });
     $(black).css({ 'opacity': '1'});
     $(white).css({ 'opacity': '0.4' });
     horizon($(this), `white`);
+    vertical($(this), `white`, id);
+    diagonal($(this), `black`, id);
   }else{
     $(ring).addClass(`black`);
-    console.log(ring);
+    // console.log(ring);
     $(ring).css({'background': 'black'});
     $(white).css({ 'opacity': '1' });
     $(black).css({ 'opacity': '0.4' });
     horizon($(this), `black`);
+    vertical($(this), `black`, id);
+    diagonal($(this), `black`, id);
   }
   move++;
 }));
@@ -50,11 +54,34 @@ function horizon(self, color){
        (self.prev().is(`.${color}`) && self.prev().prev().is(`.${color}`) && self.prev().prev().prev().is(`.${color}`)) ||
         (self.prev().is(`.${color}`) && self.prev().prev().is(`.${color}`) && self.next().is(`.${color}`))) {
     console.log(color);
-    $(`h2`).replaceWith(`<h2><span>${color} winner</span></h2>`);
+    $(`h2`).replaceWith(`<h2><span>${color} won!^_^</span></h2>`);
       game = true;
       gameAgain(game);
        return;
   }
+};
+function vertical(self, color, id){
+  if(
+    (self.parent().prev().children(`#${id}`).is(`.${color}`) && self.parent().prev().prev().children(`#${id}`).is(`.${color}`) &&
+   self.parent().prev().prev().children(`#${id}`).is(`.${color}`)) ||
+    (self.parent().next().children(`#${id}`).is(`.${color}`) && self.parent().next().children(`#${id}`).is(`.${color}`) &&
+      self.parent().next().next().next().children(`#${id}`).is(`.${color}`) )||
+    (self.parent().prev().children(`#${id}`).is(`.${color}`) && self.parent().children(`#${id}`).is(`.${color}`) &&
+      self.parent().next().next().children(`#${id}`).is(`.${color}`) )||
+    (self.parent().prev().children(`#${id}`).is(`.${color}`) && self.parent().prev().prev().children(`#${id}`).is(`.${color}`) &&
+      self.parent().children(`#${id}`).is(`.${color}`))
+  ) {
+    $(`h2`).replaceWith(`<h2><span>${color} won!^_^</span></h2>`);
+      console.log(color);
+      game = true;
+      gameAgain(game);
+      return;
+    }
+
+};
+
+function diagonal(self, color, id){
+
 };
 
 function gameAgain(game){
@@ -63,8 +90,9 @@ if(game){
  }else{
    return;
  }
-}
-  $('.btn_again').click(() => {
-    this.location.reload();
-  });
+};
+
+$('.btn_again').click(() => {
+  this.location.reload();
+ });
 });
