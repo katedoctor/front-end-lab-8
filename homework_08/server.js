c971b8a6c006df0e50a33b413c4976d7bd9889c7
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-10 04:54:27 +0300

    Add files via upload

--- homework_08/server.js
+++ homework_08/server.js
@@ -1,1 +1,15 @@
+process.stdout.write('What are you waiting for? \n');
+const express = require('express'),
+      app = express();
 
+app.use(express.json());
+var handlers = require('./controllers/handlers.js')
+
+const port = process.env.PORT || 5500;
+app.listen(port, () => console.log(`Listening to port ${port}`));
+
+app.get('/rockstars', handlers.get);
+app.get('/rockstars/:id', handlers.getId);
+app.post('/rockstars', handlers.post);
+app.put('/rockstars/:id', handlers.put);
+app.delete('/rockstars/:id', handlers.delete);
