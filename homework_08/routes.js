afb450c46e70384dda8c542621b5e7fdc172a8c4
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-10 04:43:43 +0300

    Add files via upload

--- homework_08/routes.js
+++ homework_08/routes.js
@@ -1,1 +1,10 @@
+var express = require('express'),
+    router = express.Router(),
+    handlers = require('./controllers/handlers');
 
+router.get('/rockstars', handlers.get);
+router.get('/rockstars/:id', handlers.getId);
+router.get('/rockstars', handlers.post);
+router.get('/rockstars/:id', handlers.put);
+router.get('/rockstars/:id', handlers.delete);
+module.exports = router;
