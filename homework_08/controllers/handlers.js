b51499e2eabe7c138e89ea41c72eac3e63f1c6a4
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-10 23:01:07 +0300

    Add files via upload

--- homework_08/controllers/handlers.js
+++ homework_08/controllers/handlers.js
@@ -1,13 +1,26 @@
 const fs = require('fs');
-const path = require('path');
-const rock = JSON.parse(fs.readFileSync('./data/storage.json'));
-
+var storage = './data/storage.json';
 
 exports.get = (req, res) => {
+  fs.readFile(storage, function (error, data) {
+    if (error) return res.sendStatus(404);
+    try {
+      var rock = JSON.parse(data);
+    } catch (error) {
+      return res.sendStatus(500);
+    }
   res.status(200).send(rock);
+ });
 };
 
 exports.getId = (req, res) => {
+  fs.readFile(storage, function (error, data) {
+    if (error) return res.sendStatus(500)
+    try {
+      var rock = JSON.parse(data);
+    } catch (error) {
+      return res.sendStatus(500);
+    }
  const rockstar = rock.find(c => {
    return c.id === parseInt(req.params.id);
  });
@@ -16,45 +29,72 @@ exports.getId = (req, res) => {
   };
   res.status(200).send(rockstar);
 
+});
 };
 
 exports.post = (req, res) => {
-  if (!req.body.name || !req.body.band || !req.body.instrument) {
+  fs.readFile(storage, function (error, data) {
+    if (error) return res.sendStatus(400)
+    try {
+      var rock = JSON.parse(data);
+    } catch (error) {
+      return res.sendStatus(500);
+    }
+  if (!req.body.name || !req.body.id || !req.body.band || !req.body.instrument) {
     return res.sendStatus(400).send('Data is required');
   };
-  const rockstar = rock.find(c => c.name === req.body.name);
+  const rockstar = rock.find((c)=> c.name === req.body.name);
   if(rockstar){
     return res.status(409).send({'message':'rockstar already exist'});
   };
-  rockstars = {
+  const rockstars = {
     "id": rock.length + 1,
     "name": req.body.name,
     "band": req.body.band,
     "instrument": req.body.instrument
   };
   rock.push(rockstars);
-  res.sendStatus(201).send(rockstars);
-
-};
+  res.sendStatus(201).send({ 'message': 'successfully added'});
+  res.send(rockstars);
+ });
+}
 
 exports.put = (req, res) => {
+  fs.readFile(storage, function (error, data) {
+    if (error) return res.sendStatus(500)
+    try {
+      var rock = JSON.parse(data)
+    } catch (error) {
+      return res.sendStatus(500)
+    }
   const rockstars = rock.find(c => c.id === parseInt(req.params.id));
   if(!rockstars){
     return res.status(404).send('rockstars with this id was not found');
+  };
+  if (!req.body.name || !req.body.band || !req.body.instrument) {
+    return res.sendStatus(400).send('Data is required');
   };
   rockstars.name = req.body.name;
   rockstars.band = req.body.band;
   rockstars.instrument = req.body.instrument;
   res.status(200).send(rockstars);
-
-};
+ });
+}
 
 exports.delete = (req, res) => {
-   const rockstars = rock.find(c => c.id === parseInt(req.params.id));
+  fs.readFile(storage, function (error, data) {
+    if (error) return res.sendStatus(500)
+    try {
+      var rock = JSON.parse(data)
+    } catch (error) {
+      return res.sendStatus(500)
+    }
+  const rockstars = rock.find(c => c.id === parseInt(req.params.id));
   if(!rockstars) {
     return res.status(404).send('rockstars with this id was not found');
   };
   const index = rock.indexOf(rockstars);
   rock.splice(index, 1);
   res.status(200).send({ 'message': 'Musician has been successfully removed.'});
-};
+ });
+}
