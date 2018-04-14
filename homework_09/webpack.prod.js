9d5815d6505e0824db04cdd9cc0d83fe0df2789a
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-14 22:04:51 +0300

    Update webpack.prod.js

--- homework_09/webpack.prod.js
+++ homework_09/webpack.prod.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = merge(common,{
   plugins: [
