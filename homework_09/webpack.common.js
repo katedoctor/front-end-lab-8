26ab27b62f825c63b3fa0ba75eced88eb33a36f4
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-14 15:06:10 +0300

    20%

--- homework_09/webpack.common.js
+++ homework_09/webpack.common.js
@@ -1,1 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebPlugin = require('clean-webpack-plugin');
+const HTMLWebPlugin = require('html-webpack-plugin');
 
+module.exports = {
+  context: path.resolve(_dirname, 'src'),
+  entry: {
+    'app.bundle.js' : ['./app.js', './clock/clock.js', './clock/canvasState.js'],
+    'style.css' : ['./app.scss', './clock/clock.scss'],
+    'index.html' : './app.html',
+  },
+  output: {
+    path: path.resolve(_dirname, 'bin'),
+    filename: '[name]',
+  },
+  module:{
+    rules:[{
+      test:/\.js$/,
+      use: ['babel-loader'],
+      options: { presets: ["es2015"] },
+    },
+    {
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }],
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new CleanWebPlugin(['bin']),
+    new HTMLWebPlugin({
+
+
+    })
+  ]
+};
