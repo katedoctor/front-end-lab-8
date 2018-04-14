8fce881e6d89366907eeec9af8eb64be07f6a40c
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-14 17:59:45 +0300

    20%

--- homework_09/webpack.dev.js
+++ homework_09/webpack.dev.js
@@ -1,1 +1,16 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  plugins: [
+    new UglifyJSPlugin({
+      sourceMap: true,
+    })
+  ],
+  devServer: {
+    contentBase: './bin',
+    stats: 'errors-only'
+  }
+});
