2813a7cd1bfbfb5c08a3f6b5c44b0fc678a646a2
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-04-21 22:17:34 +0300

    Add files via upload

--- homework_11/webpack.config.js
+++ homework_11/webpack.config.js
@@ -1,1 +1,41 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackRlugin = require('clean-webpack-plugin');
+const path = require('path');
 
+module.exports = {
+  context: path.resolve('src'),
+  entry: {
+    'bundle.js' : ['./js/calculating-module.js', './js/interface-module.js', './js/output-module.js'],
+    'style.css' : './styles/styles.css',
+  },
+  module: {
+    rules: [
+      {
+      test: /\.js$/,
+      use: ['babel-loader'],
+      exclude: /node_modules/,
+    },
+    {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: ['css-loader'],
+      }),
+    },
+    ]
+  },
+  plugins: [
+    new CleanWebpackRlugin(['dist']),
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin('style.css'),
+  ],
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name]'
+  },
+  watch: true
+}
