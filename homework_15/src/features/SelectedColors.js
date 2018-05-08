30e522af97537ac832e25de9eb4e190a45651728
katedoctor <astefan1669@gmail.com>
2018-05-08 05:20:05 +0300

    add counter and selected

--- homework_15/src/features/SelectedColors.js
+++ homework_15/src/features/SelectedColors.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class SelectedColors extends Component {
   render() {
     return (
-      <div className="selected-color">red
+      <div className="selected-color" style={{background:this.props.color}}>
+      aaaaaaaaa
+      {this.props.color}
       </div>
     )
   }
