8b6fff943d2b052d311e62831b8fe41fed8747eb
Kateryna Pynka <34662518+katedoctor@users.noreply.github.com>
2018-05-09 00:03:33 +0300

    Update ListofAvailableColorItems.js

--- homework_15/src/features/ListofAvailableColorItems.js
+++ homework_15/src/features/ListofAvailableColorItems.js
@@ -4,11 +4,11 @@ import React, { Component } from 'react';
 class ListofAvailableColorItems extends Component {
   render() {
     let color = this.props.color;
-    let showColor = this.props.showColor;
+    let selectedColors = this.props.selectedColors;
 
     return(
       <div className="color-item" style={{ background: this.props.color}} id={this.props.id} >
-      {showColor.length < 11 ?
+      {selectedColors.length > 10 ?
           <button className="add-button" onClick={() => this.props.onClick(color)} disabled>
             <i className="material-icons">add</i>Add</button> :
           <button className="add-button" onClick={() => this.props.onClick(color)} >
